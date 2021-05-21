859e596b37673a488d39480573d22e573386d3cc
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-05-21 15:13:36 -0300

    fix into rest client

--- src/main/java/com/catapan/restclient/IMDBAPIRestClient.java
+++ src/main/java/com/catapan/restclient/IMDBAPIRestClient.java
@@ -1,7 +1,9 @@
 package com.catapan.restclient;
 
 import com.catapan.restclient.model.MovieIMDB;
+import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -11,7 +13,9 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 @Produces(MediaType.APPLICATION_JSON)
 public interface IMDBAPIRestClient {
 
+    @GET
+    @Path("/auto-complete")
     MovieIMDB search(@HeaderParam("x-rapidapi-key") String xRapidapiKey,
-        @HeaderParam("x-rapidapi-key") String xRapidapiHost,
+        @HeaderParam("x-rapidapi-host") String xRapidapiHost,
         @QueryParam("q") String query);
 }
