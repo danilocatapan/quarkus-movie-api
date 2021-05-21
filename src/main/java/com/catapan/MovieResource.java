f2c3724e34d870a9dc23f7f2f1d166c247add362
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-05-21 15:27:59 -0300

    refactor default resource to use imdbapiRestClient

--- src/main/java/com/catapan/MovieResource.java
+++ src/main/java/com/catapan/MovieResource.java
@@ -1,16 +1,33 @@
 package com.catapan;
 
+import com.catapan.restclient.IMDBAPIRestClient;
+import com.catapan.restclient.model.MovieIMDB;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
-@Path("/hello-resteasy")
+@Path("/moveis")
 public class MovieResource {
 
+    @ConfigProperty(name = "x-rapidapi-key")
+    String xRapidapiKey;
+
+    @ConfigProperty(name = "x-rapidapi-host")
+    String xRapidapiHost;
+
+    @Inject
+    @RestClient
+    IMDBAPIRestClient imdbapiRestClient;
+
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Path("/search/{query}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public MovieIMDB search(@PathParam("query") String query) {
+        return imdbapiRestClient.search(xRapidapiKey, xRapidapiHost, query);
     }
 }
