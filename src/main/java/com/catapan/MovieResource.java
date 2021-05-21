0da68f360f4a830ca0cbf022ab648fd2a0f84d7d
Danilo Catapan <danilo.catapan@jacto.com.br>
2021-05-21 14:55:59 -0300

    rename and remove useless files

--- src/main/java/com/catapan/MovieResource.java
+++ src/main/java/com/catapan/MovieResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello-resteasy")
-public class GreetingResource {
+public class MovieResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
